24d12bb23e754fb025436396fcaa2a63b7928533
tanyaweaver <tanya83@uw.edu>
2016-05-10 14:46:15 -0700

    created 3-item array in projectItem.js

---
+++ JS/projectItems.js
@@ -0,0 +1,26 @@
+var allMyProjects = [
+  {
+    title: 'Cookie Shop',
+    publishedOn: '2016-04-21',
+    projectGhPages: 'https://tanyaweaver.github.io/Cookie-stand/',
+    projectIcon: 'assets/cookieShop.png',
+    projectDescription: 'placeholder',
+    projectSourceCode: 'https://github.com/tanyaweaver/Cookie-stand'
+  },
+  {
+    title: 'Bus Mall',
+    publishedOn: '2016-04-28',
+    projectGhPages: 'https://tanyaweaver.github.io/bus-mall/',
+    projectIcon: 'assets/cookieShop.png',
+    projectDescription: 'placeholder',
+    projectSourceCode: 'https://github.com/tanyaweaver/bus-mall'
+  },
+  {
+    title: 'Game Rock, Paper, Scissors',
+    publishedOn:'2016-05-05',
+    projectGhPages: 'https://tanyaweaver.github.io/final-project',
+    projectIcon: 'assets/cookieShop.png',
+    projectDescription: 'placeholder',
+    projectSourceCode: 'https://github.com/tanyaweaver/final-project'
+  }
+]
