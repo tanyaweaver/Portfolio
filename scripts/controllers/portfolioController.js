6312654aebf0f53e7cc2b09fdb50f40fe1324c21
tanyaweaver <tanya83@uw.edu>
2016-05-23 15:23:33 -0700

    code for portfolioController.js

---
+++ scripts/controllers/portfolioController.js
@@ -0,0 +1,14 @@
+(function(module) {
+  portfolioController ={};
+  portfolioController.index = function() {
+    if($('#project section').length === 0) {
+      Project.fetchAll();
+    }
+    $('#home-page').hide();
+    $('#resume').hide();
+    $('#projects').show();
+    $('#admin').hide();
+    $('#contact-me').hide();
+  };
+  module.portfolioController = portfolioController;
+})(window);
