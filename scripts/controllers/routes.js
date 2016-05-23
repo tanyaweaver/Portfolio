26d953843affe4027cc6f2f09991a055310224f8
tanyaweaver <tanya83@uw.edu>
2016-05-23 15:14:03 -0700

    created routes inside routes.js

---
+++ scripts/controllers/routes.js
@@ -0,0 +1,6 @@
+page('/', homeController.index);
+page('/portfolio', portfolioController.index);
+page('/about', aboutController.index);
+page('/contact', contactController.index);
+page('/admin', adminController.index);
+page();
